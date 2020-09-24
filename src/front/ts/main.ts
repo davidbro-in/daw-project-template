e681c8086106959ef2b973182e7ea58a13e5cad7
David Broin <davidmbroin@gmial.com>
2020-09-24 16:58:05 -0500

    Complete exercise 3

--- src/front/ts/main.ts
+++ src/front/ts/main.ts
@@ -12,11 +12,72 @@ let user = "TypesScript Users!";
 
 //=======[ Main module code ]==================================================
 
+
+class User {
+    private _id:number;
+    private _name:string;
+    private _email:string;
+    private _isLogged:boolean;
+    
+    constructor (id:number, name:string, email:string){
+        this._id = id;
+        this._name = name;
+        this._email = email;
+    }
+    
+    set id(id:number){
+        this._id = id;
+    }
+    
+    get id():number {
+        return this._id;
+    }
+    
+    set name(name:string){
+        this._name = name;
+    }
+    
+    get name():string {
+        return this._name;
+    }
+    
+    set email(email:string){
+        this._email = email;
+    }
+    
+    get email():string {
+        return this._email;
+    }
+    
+    printInfo():void {
+        console.log("id: " + this.id + ", nombre: " + this.name + ", email: " + this.email);
+    }
+}
+
+class Main {
+    main():void{
+        console.log("Hola mundo desde main");
+        let users = new Array<User>();
+
+        users.push(new User(1, "Juan", "juan@mail.com"));
+        users.push(new User(2, "Pedro", "juan@mail.com"));
+        users.push(new User(3, "David", "juan@mail.com"));
+        users.push(new User(4, "Ernesto", "juan@mail.com"));
+        
+        for (let i in users){
+            users[i].printInfo();
+        }
+    }
+}
+
 function greeter(person) {
     return "Hello, " + person;
  }
- 
- console.log("Hola mundo");
+
  //document.body.innerHTML = greeter(user);
 
+ window.onload = function() {
+     let myMain = new Main();
+     myMain.main();
+ }
 //=======[ End of file ]=======================================================
